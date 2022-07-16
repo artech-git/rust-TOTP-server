31c300a3031acc5a7aac409c8c9aa77d5f12ece3
artech-git <45896902+artech-git@users.noreply.github.com>
2022-07-16 21:41:09 +0530

    mutable method removed

--- src/db.rs
+++ src/db.rs
@@ -1,9 +1,7 @@
-
-use std::{collections::HashMap};
 use once_cell::sync::Lazy;
+use std::collections::HashMap;
 
-
-pub static mut DB: Lazy<HashMap<String, String>> = Lazy::new( || {
-    let mut hm: HashMap<String, String> = HashMap::new();
+pub static mut DB: Lazy<HashMap<String, String>> = Lazy::new(|| {
+    let hm: HashMap<String, String> = HashMap::new();
     return hm;
 });
