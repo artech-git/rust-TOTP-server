86dd49adee8a5979d5b2891f3b9a6f9495630e33
artech-git <45896902+artech-git@users.noreply.github.com>
2022-07-28 20:59:00 +0530

    Database type now gets returned from inside the lazy_static evaluation block using the async_once type which supports await calls inside futures block

--- src/db.rs
+++ src/db.rs
@@ -1,7 +1,19 @@
-use once_cell::sync::Lazy;
-use std::collections::HashMap;
+use async_once::AsyncOnce;
+use aws_sdk_dynamodb::{Client, Endpoint, Region};
+use http::Uri;
 
-pub static mut DB: Lazy<HashMap<String, String>> = Lazy::new(|| {
-    let hm: HashMap<String, String> = HashMap::new();
-    return hm;
-});
+lazy_static! {
+    pub static ref DB_AWS: AsyncOnce<Client> = AsyncOnce::new(async {
+        let config = aws_config::from_env().load().await;
+
+        let dynamodb_local_config = aws_sdk_dynamodb::config::Builder::from(&config)
+            .region(Region::new("ap-south-1"))
+            .endpoint_resolver(Endpoint::immutable(Uri::from_static(
+                "https://dynamodb.ap-south-1.amazonaws.com/totp-auth",
+            )))
+            .build();
+
+        let client = Client::from_conf(dynamodb_local_config);
+        return client;
+    });
+}
