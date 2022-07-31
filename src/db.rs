78c00842fc2bfde04471d5e63621b199a7c5fa0b
artech-git <45896902+artech-git@users.noreply.github.com>
2022-08-01 04:02:14 +0530

    aws dynamodb client now statically initializes values using the values from the settings.toml which are passed to it from the KEY_MAP holder
    settings.toml file now accepts aws IAM keys instead which were
    required to be set as a env variable

--- src/db.rs
+++ src/db.rs
@@ -1,31 +1,54 @@
-use async_once::AsyncOnce;
 use aws_sdk_dynamodb::{Client, Endpoint, Region};
+use aws_types::Credentials;
 use http::Uri;
+use once_cell::sync::Lazy;
 
 use crate::obj::KEY_MAP;
 
-lazy_static! {
-    pub static ref DB_AWS: AsyncOnce<Client> = AsyncOnce::new(async {
-        let config = aws_config::from_env().load().await;
+pub static DynamoDBClient: Lazy<Client> = Lazy::new(|| {
+    let region = KEY_MAP
+        .get(&"aws-region".to_string())
+        .unwrap_or(&"ap-south-1".to_string())
+        .to_owned();
 
-        let region = KEY_MAP
-            .get(&"aws-region".to_string())
-            .unwrap_or(&"ap-south-1".to_string())
-            .to_owned();
+    let url = KEY_MAP
+        .get(&"aws-region-url".to_string())
+        .unwrap_or(&format!("https://dynamodb.{}.amazonaws.com/", region))
+        .to_owned()
+        .parse::<Uri>()
+        .unwrap();
 
-        let url = KEY_MAP
-            .get(&"aws-region-url".to_string())
-            .unwrap_or(&format!("https://dynamodb.{}.amazonaws.com/", region))
-            .to_owned()
-            .parse::<Uri>()
-            .unwrap();
+    let db_key = match KEY_MAP.get(&"db-access-key".to_string()) {
+        Some(v) => {
+            tracing::log::info!("access key present");
+            v
+        }
+        None => {
+            tracing::log::warn!("access key is not present");
+            panic!();
+        }
+    };
 
-        let dynamodb_local_config = aws_sdk_dynamodb::config::Builder::from(&config)
-            .region(Region::new(region))
-            .endpoint_resolver(Endpoint::immutable(url))
-            .build();
+    let db_sec = match KEY_MAP.get(&"db-secret-access-key".to_string()) {
+        Some(v) => {
+            tracing::log::info!("secret key present");
+            v
+        }
+        None => {
+            tracing::log::warn!("secret key is not present");
+            panic!();
+        }
+    };
 
-        let client = Client::from_conf(dynamodb_local_config);
-        return client;
-    });
-}
+    let creds = Credentials::from_keys(db_key, db_sec, None);
+
+    let dynamodb_local_config = aws_sdk_dynamodb::config::Builder::new()
+        .credentials_provider(creds)
+        .region(Region::new(region))
+        .endpoint_resolver(Endpoint::immutable(url))
+        .build();
+
+    let client = Client::from_conf(dynamodb_local_config);
+
+    return client;
+});
