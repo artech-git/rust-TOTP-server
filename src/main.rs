b2dbabf5c4692b01006db170d1e317f76310a6c7
artech-git <45896902+artech-git@users.noreply.github.com>
2022-10-24 00:13:04 +0530

    routes added for otp_authentication & authorization

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use axum::{routing::get, Router};
 
 #[macro_use]
-extern crate lazy_static;
+extern crate lazy_static; //crate required for using lazy_static! macro
 
 extern crate rand;
 
@@ -12,27 +12,30 @@ mod obj;
 mod operation;
 mod test;
 
-use crate::auth::{register_user, verification};
+use crate::auth::{authentication, otp_verification, register_user, verification};
 
 #[tokio::main]
 async fn main() {
     tracing_subscriber::fmt::init();
 
+    // setup the routes which will going to be passed to the respective debug assertion
     let app = Router::new()
-        .route("/register", get(register_user))
-        .route("/verify", get(verification));
+        .route("/signin", get(register_user))
+        .route("/login", get(verification))
+        .route("/verify", get(otp_verification))
+        .route("/authorize", get(authentication));
 
-    #[cfg(debug_assertions)]
+    #[cfg(debug_assertions)] // select the following block if the --release flag is not present
     {
         axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
-            .http2_enable_connect_protocol()
-            .serve(app.into_make_service())
+            .http2_enable_connect_protocol() //enable http2 connection procedure for the axum server
+            .serve(app.into_make_service()) //serve our application on this route
             .await
             .unwrap();
     }
 
     // If we compile in release mode, use the Lambda Runtime
-    #[cfg(not(debug_assertions))]
+    #[cfg(not(debug_assertions))] // select the following code block on --release builds
     {
         // To run with AWS Lambda runtime, wrap in our `LambdaLayer`
         let app = tower::ServiceBuilder::new()
